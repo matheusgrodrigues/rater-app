4a7be4013039b27bdd2e365daa22f8dc0abec2c1
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2024-08-11 09:57:25 -0300

    Ajustando tamanho do butotn Icon

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -3,8 +3,10 @@ import styled from 'styled-components';
 const RoundedIconButton = styled.button`
     background-color: ${({ theme }) => theme.ref.colors['secondary-interactive-3']};
     border-radius: ${({ theme }) => theme.ref.borderRadius['99']};
-    height: ${({ theme }) => theme.utils.pxToRem(40)};
-    width: ${({ theme }) => theme.utils.pxToRem(40)};
+    max-height: ${({ theme }) => theme.utils.pxToRem(40)};
+    min-height: ${({ theme }) => theme.utils.pxToRem(40)};
+    min-width: ${({ theme }) => theme.utils.pxToRem(40)};
+    max-width: ${({ theme }) => theme.utils.pxToRem(40)};
     justify-content: center;
     align-items: center;
     display: flex;
