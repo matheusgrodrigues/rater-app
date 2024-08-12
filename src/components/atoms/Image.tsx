198ad2c7a55e967d30c1d39c3cbf1bd0f8e66ff6
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2024-08-12 10:31:03 -0300

    Criando component Image

---
+++ src/components/atoms/Image.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface ImageProps extends React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement> {}
+
+export default function Image({ ...props }: ImageProps) {
+    return <ImageStyled {...props} />;
+}
+
+const ImageStyled = styled.img<ImageProps>`
+    width: 100%;
+    height: 100%;
+
+    background: linear-gradient(180deg, rgba(0, 0, 0, 0) 17.12%, rgba(0, 0, 0, 0.7) 100%),
+        url('${(props) => props.src}');
+
+    background-origin: border-box;
+    background-repeat: no-repeat;
+    background-position: top;
+    background-size: cover;
+`;
--- src/components/organisms/CarrouselMovie.tsx
+++ src/components/organisms/CarrouselMovie.tsx
@@ -1,17 +1,48 @@
+import styled from 'styled-components';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import 'swiper/swiper.min.css';
+
+import Image from '../atoms/Image';
 
 interface CarrouselMovieProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
 
 export default function CarrouselMovie({ ...props }: CarrouselMovieProps) {
     return (
         <Swiper data-testid="carrousel-movie" spaceBetween={50} slidesPerView={3}>
-            <SwiperSlide>Slide 1</SwiperSlide>
-            <SwiperSlide>Slide 2</SwiperSlide>
-            <SwiperSlide>Slide 3</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
+            <SwiperSlide>
+                <CardMovie>
+                    <ImageOverride src="https://image.tmdb.org/t/p/original/yDHYTfA3R0jFYba16jBB1ef8oIt.jpg%22" />
+                </CardMovie>
+            </SwiperSlide>
+            <SwiperSlide>
+                <CardMovie>
+                    <ImageOverride src="https://image.tmdb.org/t/p/original/yDHYTfA3R0jFYba16jBB1ef8oIt.jpg%22" />
+                </CardMovie>
+            </SwiperSlide>
+            <SwiperSlide>
+                <CardMovie>
+                    <ImageOverride src="https://image.tmdb.org/t/p/original/yDHYTfA3R0jFYba16jBB1ef8oIt.jpg%22" />
+                </CardMovie>
+            </SwiperSlide>
+            <SwiperSlide>
+                <CardMovie>
+                    <ImageOverride src="https://image.tmdb.org/t/p/original/yDHYTfA3R0jFYba16jBB1ef8oIt.jpg%22" />
+                </CardMovie>
+            </SwiperSlide>
             ...
         </Swiper>
     );
 }
+
+const CardMovie = styled.div`
+    height: ${({ theme }) => theme.utils.pxToRem(284)};
+    width: ${({ theme }) => theme.utils.pxToRem(166)};
+
+    ${({ theme }) =>
+        theme.utils.screen('lg', `width: ${theme.utils.pxToRem(380)}; height: ${theme.utils.pxToRem(253)};`)}
+`;
+
+const ImageOverride = styled(Image)`
+    border-radius: ${({ theme }) => theme.ref.borderRadius['24']};
+`;
